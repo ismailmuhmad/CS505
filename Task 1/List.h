1e2ddfe4c0441707d9bd259c78a33dbfc5180537
Ismail Muhammad <ismaelmuhmad@gmail.com>
2025-11-26 00:34:03 +0000

    Task 1

---
+++ Task 1/List.h
@@ -0,0 +1,41 @@
+class List {
+
+    
+    class node {
+    public:
+        int key;
+        char data;
+        node *next;
+    }; 
+    node* head, cursor, prev;
+
+    public:
+   
+    List();
+    ~List();
+
+    bool listIsEmpty() const; 
+	bool curIsEmpty() const; 
+	void toFirst(); 
+	bool atFirst() const;
+	void advance(); 
+    void toEnd(); 
+	bool atEnd() const;
+	int  listSize() const;
+	void updateData (const char & ); 
+    void updateKey (const char & ); 
+	char retrieveData () const;
+    void retrieveData (const char &);
+	void insertFirst (const int &, const char & ); 
+	void insertAfter (const int &, const char & );
+	void insertBefore (const int &, const char & );
+	void insertEnd (const int &, const char & );
+	void deleteNode();
+	void deleteFirst();
+	void deleteEnd();
+	void makeListEmpty(); 
+	bool search (const int & ); 
+	void orderInsert(const int &, const char & );
+	void traverse();
+    
+};
